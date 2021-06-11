3cb216c6bab1afa72b5d40ba485fdcbfd0c06703
Anhad <anhadrs@gmail.com>
2021-06-12 04:27:36 +1000

    updates

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
+
+export var SPEED = 50;
+export var POINTER_RADIUS = 1;
 var vel = Vector2()
-export var speed = 50;
+var angle_to_fire = 0;
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -13,7 +16,7 @@ func _ready():
 
 
 func _process(delta):
-	var move_vec = Vector2()
+	var move_vec = Vector2();
 	if (Input.is_action_pressed("move_up")):
 		move_vec.y -=1;
 	if (Input.is_action_pressed("move_down")):
@@ -23,7 +26,23 @@ func _process(delta):
 	if (Input.is_action_pressed("move_right")):
 		move_vec.x +=1
 	move_vec = move_vec.normalized()
-	vel = move_vec * speed;
+	vel = move_vec * SPEED;
+#	var mouse_pos = get_viewport().get_mouse_position()
+	
+	
+#	print("Mouse" + str(mouse_pos))
+	
 	
 func _physics_process(delta):
 	move_and_slide(vel);
+
+func _input(event):
+#	print(event.position)
+	if (event is InputEventMouseMotion):
+		var mouse_pos = get_global_mouse_position();
+#		$Pointer.look_at(mouse_pos)
+		var dir_to_mouse = mouse_pos - position;
+		angle_to_fire = atan2(dir_to_mouse.y, dir_to_mouse.x);
+		$Pointer.rotation = angle_to_fire;
+		$Pointer.position = Vector2(cos(angle_to_fire), sin(angle_to_fire)).normalized() * POINTER_RADIUS;
+#		print("Mouse motion " + str(event.position))
